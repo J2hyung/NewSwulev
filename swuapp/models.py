b0a50e43a0d562a80600581d86ff93de023dcd0b
J2hyung <jhp0227@likelion.org>
2020-02-23 15:18:44 +0900

    models, admin 등 db관련 by. 지현, html초안 by. 지형

--- swuapp/models.py
+++ swuapp/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    userid = models.CharField(max_length = 20, unique=True, primary_key=True)
+    userpw = models.CharField(max_length = 30)
+
+    def __str__(self):
+        return self.userid
+
+class UserLecture(models.Model):
+
+    RATING_FIELD = (
+        ('on', 'on'),
+        ('off', 'off'),
+    )
+
+    myuserid = models.ForeignKey('User', on_delete=models.CASCADE, to_field='userid', related_name='myuserid', primary_key=True, unique=True)
+    mylectureid = models.ForeignKey('Lecture', to_field='lectureid',on_delete=models.CASCADE, related_name='mylectureid')
+    rating = models.CharField(max_length=10, choices=RATING_FIELD, default = "off")
+
+
+class Lecture(models.Model):
+    semester = models.CharField(max_length = 30, unique=True)
+    lectureid = models.CharField(max_length=20, primary_key=True, unique=True)
+    lecturename = models.CharField(max_length=50)
+    professor = models.CharField(max_length=50)
+
+
+class Board(models.Model):
+    content = models.TextField()
+    quality = models.IntegerField()
+    challenge = models.IntegerField()
+    recommend = models.IntegerField()
+    user = models.ForeignKey('User', on_delete=models.CASCADE, related_name='user_id')
+    lecture = models.ForeignKey('Lecture', on_delete=models.CASCADE, related_name='lecture')
--- swuapp/views.py
+++ swuapp/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
+
+def login(request):
+
+    return render(request, 'login.html')
+
+def main(request):
+
+    board = Board.objects.all
+    lecture = Lecture.objects.all
+    return render(request, 'main.html', {'board':board, 'lecture':lecture})
+
+def detail(request):
+
+    return render(request, 'detail.html')
+
+def mypage(request):
+    return render(request, 'mypage.html')
+
+def signup(request):
+    #user1 = Lecture.objects.raw('select * from swuapp_lecture')[0]
+    if request.method == 'POST':
+            user_id = request.POST['id']
+            user_pw = request.POST['passwd']
+            user = User(userid=user_id, userpw=user_pw)
+            user.save()
+            if user is not None:
+                return redirect('main')
+            else:
+                return render(request, 'signup.html', {'error': 'ID 또는 PW가 올바르지 않습니다.'})
+    else:
+        return render(request, 'signup.html')
+    # user1 = Lecture.objects.raw('select * from swuapp_lecture')[0]
+    # return render(request, 'login.html', {'user1':user1})
