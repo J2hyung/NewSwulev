1bbfc721d3b55b32b3b4ef39f6097bffb72c282f
YuRi2Song <dbfldi10@likelion.org>
2020-02-24 17:39:14 +0900

    강의평 작성 화면 완료

--- swulev/urls.py
+++ swulev/urls.py
@@ -7,9 +7,10 @@ urlpatterns = [
     path('', login, name='login'),
     path('signup', signup, name='signup'),
     path('main/', main, name='main'),
+    path('new/', new, name='new'),
+    path('create/', create, name='create'),
     path('detail/<lectureid>/', detail, name='detail'),
     path('mypage/<int:board_id>/delete/', delete, name='delete'),
     path('mypage/', mypage, name='mypage'),
     path('main_search/',post_list,name='search'),
-
 ]
